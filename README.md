71d24b54ae5102f066ce7e08ecc733bb3fc691c7
Brandon Sanderson <bas.bsanders@gmail.com>
2015-03-27 12:39:09 -0700

    Add some docs, minor readme stuff, license.

---
+++ README.md
@@ -0,0 +1,3 @@
+## GenFSM
+
+A generic finite state machine library for rust.
--- src/lib.rs
+++ src/lib.rs
@@ -21,24 +21,37 @@
  * THE SOFTWARE.
  *
  */
-#![feature(convert)]
+use std::borrow::Borrow;
 
+///Represents the next state an FSM will enter
 pub enum NextState<S,I>{
+  ///Change the state and the executor that handles input
   ChangeState(Executor<S,I>, S),
+  ///Continue with the same executor
   Continue(S),
+  ///FSM has reached completion.  Any further attempts
+  ///to handle input will result in no changes
   End(S)
 }
 
+///An input handler that executes changes on a state
 pub type Executor<S,I> = fn(S, I) -> NextState<S,I>;
 
+///The core of the GenFSM library.  Holds the Executor
+///and current state of a state machine, and handles
+///the processing of NextState values from Executors.
 pub struct StateMachine<S, I> {
+  ///The next Executor to handle input.  Will be None
+  ///if this state machine has finished.
   next_executor:Option<Executor<S,I>>,
-  ///State should only ever be None when the process
+  ///The current state.  Should only ever be None when the process
   ///method is running.
   current_state:Option<S>
 }
 
 impl<S,I> StateMachine<S,I>{
+  ///Processes new input, using the current executor
+  ///to update state.
   pub fn process(&mut self, input_val:I){
     let mut next_state_opt:Option<NextState<S,I>> = None;
     let old_state = self.current_state.take().unwrap();
@@ -68,24 +81,27 @@ impl<S,I> StateMachine<S,I>{
   pub fn is_complete(&self) -> bool{
     self.next_executor.is_none()
   }
+  ///Extracts the current state from the state machine.
   pub fn extract_state(self) -> S {
     self.current_state.unwrap()
   }
+
+  ///Creates a state machine starting with the given state.
+  ///The executor given will be used to process input.
   pub fn new(exec:Executor<S,I>, state:S) -> StateMachine<S,I>{
     StateMachine{next_executor:Some(exec), current_state:Some(state)}
   }
 }
 
-impl<S,I> AsRef<S> for StateMachine<S, I> {
-  fn as_ref(&self) -> &S {
+impl<S,I> Borrow<S> for StateMachine<S, I> {
+  fn borrow(&self) -> &S {
     self.current_state.as_ref().unwrap()
   }
 }
 
-//provide a way to get state out while processing
-//if state is cloneable
 impl<S:Clone, I> StateMachine<S,I>{
-  pub fn copy_state(&self) -> S {
+  ///Returns a clone of the current state
+  pub fn clone_state(&self) -> S {
     self.current_state.clone().unwrap()
   }
 }
@@ -94,9 +110,9 @@ impl<S:Clone, I> StateMachine<S,I>{
 fn simple_storage(){
   let storer:Executor<Option<isize>, isize> = simple_storage_statefn;
   let mut state_machine:StateMachine<Option<isize>, isize> = StateMachine::new(storer, None);
-  assert!(state_machine.copy_state() == None);
+  assert!(state_machine.clone_state() == None);
   state_machine.process(2);
-  assert!(state_machine.copy_state() == Some(2));
+  assert!(state_machine.clone_state() == Some(2));
   state_machine.process(0);
   assert!(state_machine.is_complete());
 }
